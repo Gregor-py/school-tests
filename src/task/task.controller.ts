e2f0795e1d1d6bd3c783c9528283e7db42f99048
Vitaliy <vitaliydps4@gmail.com>
2023-02-16 15:02:04 +0100

    change for deleteAnswer end point variant from delete to put

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,32 +1,31 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
-import { Creator } from '../test/decorators/creator.decorator';
-import { Auth } from '../auth/decorators/auth.decorator';
-import { IdValidationPipe } from '../pipes/id.validation.pipe';
-import { ChangeQuestionDto } from './dto/change-question.dto';
-import { TaskService } from './task.service';
-import { ChangeTaskTypeDto } from './dto/change-task-type.dto';
-import { ChangeCorrectAnswerDto } from './dto/change-correct-answer.dto';
-import { User } from '../user/decorators/user.decorator';
-import { UserModel } from '../user/model/user.model';
-import { Types } from 'mongoose';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common'
+import { ApiTags } from '@nestjs/swagger'
+import { Types } from 'mongoose'
+import { Auth } from '../auth/decorators/auth.decorator'
+import { IdValidationPipe } from '../pipes/id.validation.pipe'
+import { Creator } from '../test/decorators/creator.decorator'
+import { User } from '../user/decorators/user.decorator'
+import { UserModel } from '../user/model/user.model'
+import { ChangeCorrectAnswerDto } from './dto/change-correct-answer.dto'
+import { ChangeQuestionDto } from './dto/change-question.dto'
+import { TaskService } from './task.service'
 
 @ApiTags('tasks')
 @Controller('tasks')
 export class TaskController {
-  constructor(private taskService: TaskService) {}
+  constructor(private taskService: TaskService) { }
 
   @Auth()
   @Get('/:taskId')
   getById(@Param('taskId', IdValidationPipe) taskId) {
-    return this.taskService.getById(taskId);
+    return this.taskService.getById(taskId)
   }
 
   @Auth()
   @Creator('task')
   @Put('/question/:taskId')
   changeQuestion(@Param('taskId', IdValidationPipe) taskId, @Body() changeQuestionDto: ChangeQuestionDto) {
-    return this.taskService.changeQuestion(changeQuestionDto.newQuestion, taskId);
+    return this.taskService.changeQuestion(changeQuestionDto.newQuestion, taskId)
   }
 
   @Auth()
@@ -36,20 +35,20 @@ export class TaskController {
     @Param('taskId', IdValidationPipe) taskId,
     @Body() changeCorrectAnswerDto: ChangeCorrectAnswerDto,
   ) {
-    return this.taskService.changeCorrectAnswer(taskId, changeCorrectAnswerDto);
+    return this.taskService.changeCorrectAnswer(taskId, changeCorrectAnswerDto)
   }
 
   @Auth()
   @Creator('task')
   @Post('/answer/:taskId')
   addAnswer(@Param('taskId', IdValidationPipe) taskId, @User() user: UserModel) {
-    return this.taskService.addAnswer(taskId, user._id);
+    return this.taskService.addAnswer(taskId, user._id)
   }
 
   @Auth()
   @Creator('task')
-  @Delete('/answer/:taskId')
+  @Put('/answer/:taskId')
   deleteAnswer(@Param('taskId', IdValidationPipe) taskId, @Body('answerId') answerId: Types.ObjectId) {
-    return this.taskService.deleteAnswer(answerId, taskId);
+    return this.taskService.deleteAnswer(answerId, taskId)
   }
 }
